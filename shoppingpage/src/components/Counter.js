5cefc54efe270eb5347f758af343d6d3415c4697
Prajwal Pawar <prajwalrpawar2001@gmail.com>
2025-01-07 18:20:53 +0530

    undefined value fix it

--- shoppingpage/src/components/Counter.js
+++ shoppingpage/src/components/Counter.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Counter = () => {
+
+  const count = useSelector((state) => state.counter.value);
+
   return (
-    <div>Counter</div>
+    <div>
+      <button>
+        Increment
+      </button>
+
+      <br/>
+      <br/>
+
+      <div>{count}</div>
+
+      <br/>
+      <br/>
+
+      <button>
+        Decrement
+      </button>
+    </div>
   )
 }
 
